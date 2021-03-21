541a492449fb559f6524b59b8bccedebcc10f11f
bruno <ba160497@gmail.com>
2021-03-21 15:15:07 -0300

    ajuste de comando

--- CPU.java
+++ CPU.java
@@ -131,7 +131,7 @@ public class CPU {
 
                     case STD: // [A] ← Rs
                             m[ir.p].opc = Opcode.DATA;
-                            m[ir.p].p = reg[ir.r2];
+                            m[ir.p].p = reg[ir.r1];
                             pc++;
                         break;
 
