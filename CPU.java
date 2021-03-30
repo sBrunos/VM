54fa18c1be2ad0688fa1ab4225ac79e9cf6de50d
Bruno Andrade <ba160497@gmail.com>
2021-03-30 12:53:49 -0300

    Interrupções implementadas

--- CPU.java
+++ CPU.java
@@ -16,8 +16,24 @@ public class CPU {
         pc = _pc; // limite e pc (deve ser zero nesta versao)
     }
 
+    public boolean verifyOutOfBounds(int i){
+
+        if(i > 1023){
+            return true;
+        }
+        
+        return false;
+    }
+
+
     public void run() { 		// execucao da CPU supoe que o contexto da CPU, vide acima, esta devidamente setado
-			while (true) { 			// ciclo de instrucoes. acaba cfe instrucao, veja cada caso.
+            while (true) { 			// ciclo de instrucoes. acaba cfe instrucao, veja cada caso.
+                
+
+                boolean invalidIntruction = false;
+                boolean outOfBounds = false;
+                boolean overflow = false;
+
 				// FETCH
 					ir = m[pc]; 	// busca posicao da memoria apontada por pc, guarda em ir
 				// EXECUTA INSTRUCAO NO ir
@@ -52,12 +68,16 @@ public class CPU {
                         break;
 
                     case JMPIM: 
+                        outOfBounds = verifyOutOfBounds(ir.p);
+                        if(outOfBounds) break;
                         pc = m[ir.p].p;
 
                         break;
 
                     case JMPIGM: 
                         if(ir.r2 > 0){
+                            outOfBounds = verifyOutOfBounds(ir.p);
+                            if(outOfBounds) break;
                             pc = m[ir.p].p;
                         }
                         else{
@@ -68,6 +88,8 @@ public class CPU {
                     
                     case JMPILM: 
                         if(ir.r2 < 0){
+                            outOfBounds = verifyOutOfBounds(ir.p);
+                            if(outOfBounds) break;
                             pc = m[ir.p].p;
                         }
                         else{
@@ -77,6 +99,9 @@ public class CPU {
 
                     case JMPIEM:
                         if(ir.r2 == 0){
+
+                            outOfBounds = verifyOutOfBounds(ir.p);
+                            if(outOfBounds) break;
                             pc = m[ir.p].p;
                         }
                         else{
@@ -130,17 +155,23 @@ public class CPU {
                         break;
 
                     case STD: // [A] ← Rs
+                            outOfBounds = verifyOutOfBounds(ir.p);
+                            if(outOfBounds) break;
                             m[ir.p].opc = Opcode.DATA;
                             m[ir.p].p = reg[ir.r1]; // Erro resolvido
                             pc++;
                         break;
 
                     case LDX: 
+                        outOfBounds = verifyOutOfBounds(ir.r2);
+                        if(outOfBounds) break;
                         reg[ir.r1] = m[ir.r2].p;
                         pc++;
                         break;
 
                     case STX: // [Rd] ←Rs
+                        outOfBounds = verifyOutOfBounds(reg[ir.r1]);
+                        if(outOfBounds) break;
                             m[reg[ir.r1]].opc = Opcode.DATA;      
                             m[reg[ir.r1]].p = reg[ir.r2];          
                             pc++;
@@ -154,10 +185,32 @@ public class CPU {
                             pc++;
                         break;
 
+                    default : // Instrução inválida 
+                            invalidIntruction = true;
+                            break;
+
 					
 //#endregion
 
             }
+
+                //Verificação de interrupções:
+
+                if(invalidIntruction){
+                    System.out.println("Instrução Inválida");
+                    break;
+                }
+
+                if(outOfBounds){
+                    System.out.println("Endereço inválido");
+                    break;
+                }
+
+                if(overflow){
+                    System.out.println("Overflow em operação matemática");
+                    break;
+                }
+                
 				// VERIFICA INTERRUPÇÃO !!! - TERCEIRA FASE DO CICLO DE INSTRUÇÕES
 				if (ir.opc==Opcode.STOP) {   
 					break; // break sai do loop da cpu
